ab9ea69e18498de6f2047abf0ba84bda303b0bf3
Noom07 <nomena.dev@gmail.com>
2025-05-16 17:46:04 +0300

    Feat : fecth les données via url (api) et async/await (Asynchrone)

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import characters  from "./modules/characters.js"
 import displayPersonnages from "./modules/functions.js"
 displayPersonnages(characters)
+
+
+async function getData(){
+  const response = await fetch("https://myapis.barpat.fun/api_characters_rpg")
+  console.log(response)
+}
+
+getData()
